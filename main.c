00c57a1d73d394044d40151a273a63334deffd8f
Rusty <rusty3925@gmail.com>
2026-03-13 15:39:58 -0700

    added decode

--- main.c
+++ main.c
@@ -6,6 +6,10 @@
 
 #include <assert.h>
 
+static inline int min(int x, int y) {
+    return x > y ? x : y;
+}
+
 static struct options {
     bool decode;
 } options = { .decode = false };
@@ -49,8 +53,31 @@ int decode(FILE* restrict from, FILE* restrict to) {
     char out[3];
     int read_ret;
     int write_ret;
-    while ((read_ret = fread(in, 1, 4, from))) {
+    while ((read_ret = fread(in, 1, 4, from)) == 4) {
+        if (memchr(in, '=', 4)) {
+            break;
+        }
+        decode_chunk_full(out, in);
+        write_ret = fwrite(out, 1, 3, to);
+        if (write_ret < 3) {
+            if (ferror(to)) {
+                return -1;
+            }
+        }
+    }
 
+    if (read_ret) {
+        if (read_ret == 4) {
+            decode_chunk_padding(out, in);
+        } else if (read_ret == 1 || read_ret == 2 || read_ret == 3) {
+            decode_chunk_partial(out, in, read_ret);
+        }
+        write_ret = fwrite(out, 1, 3, to);
+        if (write_ret < 3) {
+            if (ferror(to)) {
+                return -1;
+            }
+        }
     }
     return 1;
 }
