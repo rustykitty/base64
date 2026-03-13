fe3d5a589c877fdec2e845f64cc244c9bc405e27
Rusty <rusty3925@gmail.com>
2026-03-12 19:34:41 -0700

    fix test_base64

--- test_base64.c
+++ test_base64.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-#include "base64.c"
+#include <string.h>
+#include "base64.h"
 // quick test
 int main(void) {
     char* input = "000";
     char out[4];
-    encode_block_full(out, input);
+    encode_chunk_full(out, input);
     // Should be MDAw
     printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
 
     input = "R";
-    encode_block_partial(out, input, 1);
+    encode_chunk_partial(out, input, 1);
     // should be Ug==
     printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
 
     input = "AA";
-    encode_block_partial(out, input, 2);
+    encode_chunk_partial(out, input, 2);
     // should be QUE=
     printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
 
     input = "YWJj";
-    decode_block_full(out, input);
+    decode_chunk_full(out, input);
     // Should be abc
     printf("%c%c%c\n", out[0], out[1], out[2]);
 
     // this is a shorter input- callers responsibility to clear output buf here
     input = "YXg=";
     memset(out, 0, 4);
-    decode_block_padding(out, input);
+    decode_chunk_padding(out, input);
     // Should be ax
     printf("%c%c%c\n", out[0], out[1], out[2]);
 }
