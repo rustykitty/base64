a0779c054f78d15900f6891e76c731e021af44f3
Rusty <rusty3925@gmail.com>
2026-03-13 20:45:31 -0700

    Squashed commit of the following:
    commit 93f1a5406091b095947da4cb3aa34ea735f92aeb
    Author: Rusty <rusty3925@gmail.com>
    Date:   Fri Mar 13 20:43:05 2026 -0700
    
        apply to encode_chunk_partial
    
    commit 3c7b02d99637a2ff62804a6208e6a2baa0966a6b
    Author: Rusty <rusty3925@gmail.com>
    Date:   Fri Mar 13 20:42:28 2026 -0700
    
        clearer names
    
    commit 25fa8f229e60b0f085f40f989f4b603457fb11ef
    Author: Rusty <rusty3925@gmail.com>
    Date:   Fri Mar 13 20:39:28 2026 -0700
    
        fix: actually handle byte values over 7f correctly (why is char signed in gcc)
    
    commit 22506d3a015b22d90e4edc22be4b19c4a801cee5
    Author: Rusty <rusty3925@gmail.com>
    Date:   Fri Mar 13 20:29:27 2026 -0700
    
        weird
    
        if I remove the unsigned char cast then in[2] will be 0xffffff80 (?!?)
    
    commit 91c50675054cbace6cbcaa7df3a7ff4318bd94b2
    Author: Rusty <rusty3925@gmail.com>
    Date:   Fri Mar 13 20:09:27 2026 -0700
    
        test for current bug

--- test_base64.c
+++ test_base64.c
@@ -3,31 +3,9 @@
 #include "base64.h"
 // quick test
 int main(void) {
-    char* input = "000";
+    char input[3] = { 0x00, 0x00, 0x80 };
     char out[4];
     encode_chunk_full(out, input);
-    // Should be MDAw
+    // Should be AACA
     printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
-
-    input = "R";
-    encode_chunk_partial(out, input, 1);
-    // should be Ug==
-    printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
-
-    input = "AA";
-    encode_chunk_partial(out, input, 2);
-    // should be QUE=
-    printf("%c%c%c%c\n", out[0], out[1], out[2], out[3]);
-
-    input = "YWJj";
-    decode_chunk_full(out, input);
-    // Should be abc
-    printf("%c%c%c\n", out[0], out[1], out[2]);
-
-    // this is a shorter input- callers responsibility to clear output buf here
-    input = "YXg=";
-    memset(out, 0, 4);
-    decode_chunk_padding(out, input);
-    // Should be ax
-    printf("%c%c%c\n", out[0], out[1], out[2]);
 }
