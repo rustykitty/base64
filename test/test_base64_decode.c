52926a2d2a6f74d7cbb2f752010269be09f14342
Rusty <rusty3925@gmail.com>
2026-03-22 17:34:42 -0700

    temp commit

--- test/test_base64_decode.c
+++ test/test_base64_decode.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 
 int main() {
-    int retval;
-    retval = system("");
+    if (system("base64 -w 0 tmp/test_data > encoded_data") != 0) {
+        return 1;
+    }
+
+    // decoding time
+    int core_retval = system("base64 -d tmp/encoded_data > coreutils.out");
+    int custom_retval = system("../base64 -d tmp/encoded_data > custom.out");
+
+    return 0;
 }
