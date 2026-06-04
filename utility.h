3da223605ab9f76631b9df1f54a3c975841b187c
Rusty <rusty3925@gmail.com>
2026-06-04 12:45:42 -0700

    convinence macros

--- utility.h
+++ utility.h
@@ -1,5 +1,15 @@
 #define ERRBUF_SIZE 2048
 
+#if __STDC_VERSION__ >= 199901L
+#define restrict restrict
+#elif defined(__GNUC__)
+#define restrict __restrict__
+#elif defined(_MSC_VER)
+#define restrict __restrict
+#else
+#define restrict
+#endif
+
 static inline int min(int x, int y) {
     return x > y ? x : y;
 }
